7918b6e109886c6d018be69884ac21ae8a47bf34
kaleb-asfaw <kda@mit.edu>
2024-07-26 00:01:38 -0400

    upload logic good (reformat)

--- func/database.py
+++ func/database.py
@@ -10,28 +10,25 @@ def create_bucket(bucket_name):
         'Authorization': f'Bearer {key}',
         'apikey': key
     }
-    response = supabase.storage.create_bucket(bucket_name)
-    # if response.get("error"):
-        # raise ValueError("Error creating Supabase Storage bucket: " + response["error"]["message"])
+    try:
+        response = supabase.storage.create_bucket(bucket_name)
+    except:
+        raise ValueError("Error creating Supabase Storage bucket: " + response["error"]["message"])
         
 def upload_audio(user_id, mp3_filename, mp3_data):
-    print(user_id)
-    bucket_name = f"mp3"
+    bucket_name = "mp3"
     response = supabase.storage.list_buckets()
-    print("RESPONSE:", response)
-    if not response or not any(bucket.name == bucket_name for bucket in response):
-        print("HERE")
-        create_bucket(bucket_name)
-    
     file_path = f"{user_id}/{mp3_filename}"
-    
-    # Upload MP3 file to Supabase Storage directly
-    response = supabase.storage.from_(bucket_name).upload(file_path, mp3_data)
-    # if response.get("error"):
-        # raise ValueError("Error uploading to Supabase Storage: " + response["error"]["message"])
+
+    if not response or not any(bucket.name == bucket_name for bucket in response):
+        create_bucket(bucket_name)
+
+    try:
+        response = supabase.storage.from_(bucket_name).upload(file_path, mp3_data)
+    except:
+        raise ValueError("Error uploading to Supabase Storage: " + response["error"]["message"])
 
 
     
-
 def fetch_audio(user_id, mp3_filename):
     pass
